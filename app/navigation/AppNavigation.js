f693bf37c4e4cc99f057a47753e6e7862f0b4ae9
Mahmoud Hemaid <muui-1@hotmail.com>
2020-04-02 16:00:42 +0300

    Medo [ADD] new screens (SelectAction, Categories, Search)

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SCREEN_KEYS } from "../utilities/Constants";
+import { Colors } from "../constants";
+import Context from "./Context";
 import styles from "./styles/NavigationStyles";
+import { MainTabBarNavigator } from "./stacks";
 // eslint-disable-next-line no-unused-vars
-import { GetStartedScreen, CreateAccountScreen, LogInScreen } from "../screens";
+import {
+  GetStartedScreen,
+  CreateAccountScreen,
+  LogInScreen,
+  SelectActionScreen,
+  SearchScreen
+} from "../screens";
 
 const AuthStack = createStackNavigator();
-
-const AuthStackCreen = () => (
+const AuthStackScreen = () => (
   <AuthStack.Navigator
     initialRouteName={SCREEN_KEYS.GET_STARTED}
     screenOptions={{ headerShown: false }}
@@ -25,8 +33,54 @@ const AuthStackCreen = () => (
   </AuthStack.Navigator>
 );
 
-export default () => (
-  <NavigationContainer>
-    <AuthStackCreen />
-  </NavigationContainer>
+const AppStack = createStackNavigator();
+const AppStackScreen = () => (
+  <AppStack.Navigator
+    initialRouteName={SCREEN_KEYS.SELECT_ACTION}
+    screenOptions={{ headerShown: false }}
+  >
+    <AppStack.Screen
+      name={SCREEN_KEYS.SELECT_ACTION}
+      component={SelectActionScreen}
+    />
+    <AppStack.Screen
+      name={SCREEN_KEYS.MAIN_STACK}
+      component={MainTabBarNavigator}
+    />
+    <AppStack.Screen name={SCREEN_KEYS.SEARCH} component={SearchScreen} />
+  </AppStack.Navigator>
 );
+
+const RootStack = createStackNavigator();
+const RootStackScreen = ({ isLogged }) => (
+  <RootStack.Navigator screenOptions={{ headerShown: false }}>
+    {isLogged ? (
+      <RootStack.Screen name={"App"} component={AppStackScreen} />
+    ) : (
+      <RootStack.Screen name={"Auth"} component={AuthStackScreen} />
+    )}
+  </RootStack.Navigator>
+);
+
+export default () => {
+  const [isLogged, setIsLogged] = useState(false);
+  const context = useMemo(() => {
+    return {
+      auth: {
+        logIn: () => {
+          setIsLogged(true);
+        },
+        logOut: () => {
+          setIsLogged(false);
+        }
+      }
+    };
+  }, []);
+  return (
+    <Context.Provider value={context}>
+      <NavigationContainer>
+        <RootStackScreen isLogged={isLogged} />
+      </NavigationContainer>
+    </Context.Provider>
+  );
+};
--- app/screens/index.js
+++ app/screens/index.js
@@ -1,5 +1,19 @@
+import Screen from "./Screen";
+
 import CreateAccountScreen from "./auth/CreateAccountScreen";
 import GetStartedScreen from "./auth/GetStartedScreen";
 import LogInScreen from "./auth/LogInScreen";
 
-export { CreateAccountScreen, GetStartedScreen, LogInScreen };
+import SelectActionScreen from "./main/SelectActionScreen";
+import CategoriesScreen from "./main/CategoriesScreen";
+import SearchScreen from "./main/SearchScreen";
+
+export {
+  Screen,
+  CreateAccountScreen,
+  GetStartedScreen,
+  LogInScreen,
+  SelectActionScreen,
+  CategoriesScreen,
+  SearchScreen
+};
