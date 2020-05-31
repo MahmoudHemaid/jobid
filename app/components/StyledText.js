65c392a496291eb7c12d214a49659aba2f23897b
Mahmoud Hemaid <muui-1@hotmail.com>
2020-05-31 05:36:02 +0300

    Medo [Ref] StylesText compnnet

--- app/components/StyledText.js
+++ app/components/StyledText.js
@@ -3,27 +3,32 @@ import { Text, ViewPropTypes, TouchableOpacity } from "react-native";
 import { Fonts, Colors } from "../constants";
 import PropTypes from "prop-types";
 
-const TextComponent = props => (
+const TextComponent = (props) => (
   <Text
     {...props}
     style={[
       {
+        textAlign: props.center ? "center" : "left",
         fontFamily: props.bold ? Fonts.bold : Fonts.regular,
         fontSize: props.size,
-        color: props.color
+        color: props.color,
       },
-      props.style
+      props.style,
     ]}
   />
 );
-export default function StyledText(props) {
+export default function StyledText({ onPress, ...props }) {
   if (!props.touchable) {
     return <TextComponent {...props} />;
   }
   const { containerProps, containerStyle } = props;
 
   return (
-    <TouchableOpacity {...containerProps} style={containerStyle}>
+    <TouchableOpacity
+      onPress={onPress}
+      {...containerProps}
+      style={containerStyle}
+    >
       <TextComponent {...props} />
     </TouchableOpacity>
   );
@@ -31,20 +36,22 @@ export default function StyledText(props) {
 
 StyledText.propTypes = {
   bold: PropTypes.oneOf([true, false, undefined]),
+  center: PropTypes.oneOf([true, false, undefined]),
   size: PropTypes.number,
   color: PropTypes.string,
   containerProps: PropTypes.object,
   touchable: PropTypes.oneOf([true, false, undefined]),
   containerStyle: ViewPropTypes.style,
-  children: PropTypes.string
+  children: PropTypes.string,
 };
 
 StyledText.defaultProps = {
   bold: false,
+  center: false,
   size: Fonts.size.regular,
   color: Colors.black,
   containerProps: {},
   touchable: false,
   containerStyle: {},
-  children: ""
+  children: "",
 };
